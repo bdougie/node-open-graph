69a6bcad169959a31a28b6bd740bd0fff46666b4
brian <ilikerobot@gmail.com>
2021-11-10 21:35:23 -0800

    esm

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
-var og = require('./');
-
-og("http://github.com/samholmes/node-open-graph/raw/master/test.html", function(err, meta) {
-	if (err) return console.log(err);
-	
-	console.log(meta);
-})
+import * as og from "./index.js";
+og("http://github.com/samholmes/node-open-graph/raw/master/test.html", function (err, meta) {
+    if (err)
+        return console.log(err);
+    console.log(meta);
+});
